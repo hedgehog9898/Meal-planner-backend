7466860207c31c5688287b7379d618bdb941d578
Vladimir Slepchenko <krokodainer9090@gmail.com>
2024-04-15 21:55:44 +0200

    Move strapi config to js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,14 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   proxy: true,
-  url: env('MY_HEROKU_URL'), // Sets the public URL of the application.
+  host: "0.0.0.0",
+  port: process.env.PORT,
+  url: env('MY_HEROKU_URL'),
   app: {
     keys: env.array('APP_KEYS')
   },
-});
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+  },
+})
